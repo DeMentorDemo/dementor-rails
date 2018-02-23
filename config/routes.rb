66d88234cb388117a12bf6fabf97927f1852a6dc
denys <1995gdv@gmail.com>
2018-02-23 19:45:11 +0200

    JWT setup

---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  post 'auth_user' => 'authentication#authenticate_user'
+end
