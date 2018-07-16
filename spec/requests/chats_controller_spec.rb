573e84e14c1cae7a292bd566717aa3265b9db09e
denys <1995gdv@gmail.com>
2018-07-16 16:53:17 +0300

    Test for chat creation

--- spec/requests/chats_controller_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-describe ChatsController, type: :controller do
-end
---
+++ spec/requests/chats_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Chats', type: :request do
+  let(:current_user) { create :user }
+  let(:chat_with_user) { create :user }
+  let(:valid_token) do
+    exp = 7.days.from_now.to_i
+    JsonWebToken.encode(user_id: current_user.id, exp: exp)
+  end
+
+  describe 'POST chats/create' do
+    context 'with valid attributes' do
+      it 'should create new chat' do
+        post '/chats/create', params: { user_id: chat_with_user.id }, headers: { Authorization: valid_token }
+        expect(response).to be_created
+      end
+    end
+  end
+end
