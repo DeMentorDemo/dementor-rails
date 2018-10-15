e796b4a7b4d82acfef822c0bb3038a174fdae08f
denys <1995gdv@gmail.com>
2018-10-16 01:59:50 +0300

    Refactor message broadcasting

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -12,7 +12,7 @@ module Api
     def create
       message = Message.new message_params
       if message.save
-        broadcast_message(message)
+        Messages::Broadcast.call message
         head :created
       else
         head :bad_request
@@ -20,14 +20,6 @@ module Api
     end
 
     private
-
-    def broadcast_message(message)
-      ActionCable.server.broadcast build_room_id(message.chat_id), MessageSerializer.new(message).serializable_hash
-    end
-
-    def build_room_id(id)
-      "ChatRoom-#{id}"
-    end
 
     def message_params
       params.require(:message).permit(:user_id, :chat_id, :text)
